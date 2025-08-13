2b2c15e5b3080d5f741229b0a43edf6eca41e73c
Leon Tepe <leontepe@icloud.com>
2025-08-13 10:07:22 +0200

    5.4 done

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,71 @@
 module Main where
 
+import OptParse
 import qualified HsBlog
 
+import System.Exit (exitFailure)
+import System.Directory (doesFileExist)
+import System.IO
+
+yesNoString :: Maybe Bool -> String
+yesNoString defaultValue =
+  case defaultValue of
+    Just True -> " (Y/n)"
+    Just False -> " (y/N)"
+    Nothing -> " (y/n)"
+
+ask :: String -> Maybe Bool -> IO Bool
+ask question defaultValue = do
+  putStrLn (question <> yesNoString defaultValue)
+  answer <- getLine
+  case answer of
+    "y" -> pure True
+    "n" -> pure False
+    _ ->
+      case defaultValue of
+        Just bool -> case answer of
+          -- empty string is Enter
+          "" -> pure bool
+          _ -> do
+            putStrLn "Invalid response. Type 'y' for yes, 'n' for no, or Enter to accept the default."
+            ask question defaultValue
+        
+        Nothing -> do
+          putStrLn "Invalid response. Type 'y' for yes, 'n' for no."
+          ask question defaultValue
+
 main :: IO ()
-main = HsBlog.main
+main = do
+  options <- parse
+  case options of
+    ConvertDir input output ->
+      HsBlog.convertDirectory input output
+
+    ConvertSingle input output -> do
+      (title, inputHandle) <-
+        case input of
+          Stdin ->
+            pure ("", stdin)
+          InputFile file ->
+            (,) file <$> openFile file ReadMode
+
+      outputHandle <-
+        case output of
+          Stdout -> pure stdout
+          OutputFile file -> do
+            exists <- doesFileExist file
+            shouldOpenFile <-
+              if exists
+                then ask "The output file already exists. Do you want to overwrite it?" (Just False)
+                else pure True
+            if shouldOpenFile
+              then
+                openFile file WriteMode
+              else
+                do
+                  putStrLn "Aborted."
+                  exitFailure
+
+      HsBlog.convertSingle title inputHandle outputHandle
+      hClose inputHandle
+      hClose outputHandle
--- src/HsBlog.hs
+++ src/HsBlog.hs
@@ -1,61 +1,23 @@
-{-# LANGUAGE LambdaCase #-}
-
 module HsBlog
-  ( main
+  ( convertSingle
+  , convertDirectory
   , process
   )
   where
 
-import HsBlog.Convert (process)
+import qualified HsBlog.Markup as Markup
+import qualified HsBlog.Html as Html
+import HsBlog.Convert (convert)
 
-import System.Directory (doesFileExist)
-import System.Environment (getArgs)
+import System.IO (hGetContents, hPutStrLn, Handle)
 
-yesNoString :: Maybe Bool -> String
-yesNoString defaultValue =
-  case defaultValue of
-    Just True -> " (Y/n)"
-    Just False -> " (y/N)"
-    Nothing -> " (y/n)"
+convertSingle :: Html.Title -> Handle -> Handle -> IO ()
+convertSingle title input output = do
+  content <- hGetContents input
+  hPutStrLn output (process title content)
 
-ask :: String -> Maybe Bool -> IO Bool
-ask question defaultValue = do
-  putStrLn (question <> yesNoString defaultValue)
-  answer <- getLine
-  case answer of
-    "y" -> pure True
-    "n" -> pure False
-    _ ->
-      case defaultValue of
-        Just bool -> case answer of
-          -- empty string is Enter
-          "" -> pure bool
-          _ -> do
-            putStrLn "Invalid response. Type 'y' for yes, 'n' for no, or Enter to accept the default."
-            ask question defaultValue
-        
-        Nothing -> do
-          putStrLn "Invalid response. Type 'y' for yes, 'n' for no."
-          ask question defaultValue
+convertDirectory :: FilePath -> FilePath -> IO ()
+convertDirectory = error "Not implemented"
 
-main :: IO ()
-main = getArgs >>= \case
-  -- read from stdin, write to stdout
-  [] -> do
-    stdin <- getContents
-    putStrLn (process "Empty title" stdin)
-
-  -- input/output filenames
-  [inputFileName, outputFileName] -> do
-    content <- readFile inputFileName
-    outputExists <- doesFileExist outputFileName
-    let processThenWrite = writeFile outputFileName (process "Empty title" content)
-    if outputExists
-      then do
-        overwriteOutput <- ask "The output file already exists. Do you want to overwrite it?" (Just False)
-        if overwriteOutput
-          then processThenWrite
-          else putStrLn "Aborted."
-      else processThenWrite
-
-  _ -> putStrLn "Usage: runghc Main.hs [-- <input-file> <output-file>]"
+process :: Html.Title -> String -> String
+process title = Html.render . convert title . Markup.parse
